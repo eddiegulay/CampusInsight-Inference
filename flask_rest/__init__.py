cef2ee9e7903cc45828d88d0d9fe153e63de1ed2
eddygulled <edgargulay69@gmail.com>
2024-02-10 03:56:21 +0300

    added CORS

--- flask_rest/__init__.py
+++ flask_rest/__init__.py
@@ -1,7 +1,9 @@
 # __init__.py
 from flask import Flask
+from flask_cors import CORS
 from .inference import load_models, make_inference
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 from flask_rest import routes
